99f159f6be529b43c84978e4ec202825cd02911c
ririki <ririki727@gmail.com>
2019-06-29 23:47:03 +0900

    expess4 suite

--- index.js
+++ index.js
@@ -31,6 +31,34 @@ var allowCrossDomain = function(req, res, next) {
   }
 };
 
+var connection;
+
+function handleDisconnect() {
+    console.log('INFO.CONNECTION_DB: ');
+    connection = mysql.createConnection(db_config);
+    
+    //connection取得
+    connection.connect(function(err) {
+        if (err) {
+            console.log('ERROR.CONNECTION_DB: ', err);
+            setTimeout(handleDisconnect, 1000);
+        }
+    });
+    
+    //error('PROTOCOL_CONNECTION_LOST')時に再接続
+    connection.on('error', function(err) {
+        console.log('ERROR.DB: ', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('ERROR.CONNECTION_LOST: ', err);
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
 app.get("/", function(req, res) {
   res.send("go to /posts to see posts");
 });
