6cf018b2c509335e9b733a05b2895fc8f7c0ff1b
ririki <ririki727@gmail.com>
2019-06-29 14:59:45 +0900

    ふぁすこみ

---
+++ index.js
@@ -0,0 +1,185 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  user: "b1a74395c03649",
+  password: "398a7283",
+  database: "heroku_af231fb6f51835a",
+  timezone: 'jst'
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.send("go to /posts to see posts");
+});
+
+app.get("/posts", function(req, res) {
+  console.log("select * from todos order by status , limit_date");
+  connection.query("select * from todos order by status , limit_date", function(
+    error,
+    results,
+    fields
+  ) {
+    console.log(results);
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.get("/categorys", function(req, res) {
+  console.log("select * from category");
+  connection.query("select * from category", function(
+    error,
+    results,
+    fields
+  ) {
+    console.log(results);
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/categorys/category", function(req, res) {
+  console.log("select * from category where id = " + req.body.id);
+  connection.query("select * from category where id = " + req.body.id, function(
+    error,
+    results,
+    fields
+  ) {
+    console.log(results);
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/posts/category", function(req, res) {
+  console.log('select * from todos where category = ' + req.body.category + ' order by status , limit_date');
+  connection.query('select * from todos where category = ' + req.body.category + ' order by status , limit_date', function(
+    error,
+    results,
+    fields
+  ) {
+    console.log(results);
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/posts/edit", function(req, res) {
+  console.log('select * from todos where id = ' + req.body.id);
+  connection.query('select * from todos where id = ' + req.body.id, function(
+    error,
+    results,
+    fields
+  ) {
+    console.log(results);
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/insert", function(req, res) {
+  console.log('insert into todos (title, content, limit_date, category, status) values ("' + req.body.title + '", "' + req.body.content + '", "' + req.body.limit + '", ' + req.body.category + ', 0)');
+  connection.query('insert into todos (title, content, limit_date, category, status) values ("' + req.body.title + '", "' + req.body.content + '", "' + req.body.limit + '", ' + req.body.category + ', 0)', function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/update", function(req, res) {
+  console.log('update todos set title = "' + req.body.title + '", content = "' + req.body.content + '", limit_date = "' + req.body.limit + '", category = ' + req.body.category + ' where id = ' + req.body.id);
+  connection.query('update todos set title = "' + req.body.title + '", content = "' + req.body.content + '", limit_date = "' + req.body.limit + '", category = ' + req.body.category + ' where id = ' + req.body.id, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/category/update", function(req, res) {
+  console.log('update category set title = "' + req.body.category + '" where id = ' + req.body.id);
+  connection.query('update category set title = "' + req.body.category + '" where id = ' + req.body.id, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/category/insert", function(req, res) {
+  console.log('insert into category (title) values ("' + req.body.title + '")');
+  connection.query('insert into category (title) values ("' + req.body.title + '")', function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/category/delete", function(req, res) {
+  console.log('delete from category where id = "' + req.body.id + '"');
+  connection.query('delete from category where id = "' + req.body.id + '"', function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/update/status", function(req, res) {
+  console.log('update todos set status = ' + req.body.status + ' where id = ' + req.body.id);
+  connection.query('update todos set status = ' + req.body.status + ' where id = ' + req.body.id, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.post("/delete", function(req, res) {
+  console.log('delete from todos where id = "' + req.body.id + '"');
+  connection.query('delete from todos where id = "' + req.body.id + '"', function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+
+app.listen(process.env.PORT || 4000, function() {
+  console.log("Example app listening on port 4000!");
+});
+
