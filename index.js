24f60bf0e4a9519cb6f442039e7f851ec8216879
ririki <ririki727@gmail.com>
2019-06-29 21:29:45 +0900

    cors対策

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -29,16 +29,13 @@ app.use(function(req, res, next) {
   else {
     next();
   }
-});
-
-app.configure(function () {
-  app.use(allowCrossDomain);
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+};
+app.use(allowCrossDomain);
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(application_root, "public")));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 app.get("/", function(req, res) {
   res.send("go to /posts to see posts");
